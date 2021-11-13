5478ce8f7c9721f5925108f04a2c50e59565032f
dalorke <dalorkee@gmail.com>
2021-11-13 15:45:02 +0700

    edit backend

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,7 +13,7 @@
 	<body class="font-sans antialiased">
 		<x-jet-banner />
 		<div class="min-h-screen bg-gray-100">
-			@livewire('navigation-menu')
+			{{-- @livewire('navigation-menu') --}}
 			<!-- Page Heading -->
 			@if (isset($header))
 				<header class="bg-white shadow">
--- resources/views/vendor/jetstream/components/welcome.blade.php
+++ resources/views/vendor/jetstream/components/welcome.blade.php
@@ -1,4 +1,4 @@
-<div class="p-6 sm:px-20 bg-white border-b border-gray-200">
+{{-- <div class="p-6 sm:px-20 bg-white border-b border-gray-200">
     <div>
         <x-jet-application-logo class="block h-12 w-auto" />
     </div>
@@ -10,7 +10,7 @@
     <div class="mt-6 text-gray-500">
         This is a book
     </div>
-</div>
+</div> --}}
 
 <div class="bg-gray-200 bg-opacity-25 grid grid-cols-1 md:grid-cols-2">
     <div class="p-6">
