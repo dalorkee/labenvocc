27faec65cbe70f2f65ff8f728cd098f1acb7e825
dalorkee <dalorkee@gmail.com>
2021-11-29 13:53:42 +0700

    solve login

--- app/View/Components/adminHome.php
+++ app/View/Components/adminHome.php
@@ -4,7 +4,7 @@ namespace App\View\Components;
 
 use Illuminate\View\Component;
 
-class admin-home extends Component
+class adminHome extends Component
 {
     /**
      * Create a new component instance.
