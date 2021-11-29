3fa46b31d587e201266b41bc0e41f1f41f1f3b65
dalorkee <dalorkee@gmail.com>
2021-11-29 14:44:29 +0700

    solve login

--- app/Http/Responses/LoginResponse.php
+++ app/Http/Responses/LoginResponse.php
@@ -3,17 +3,33 @@
 namespace App\Http\Responses;
 
 use Laravel\Fortify\Contracts\LoginResponse as ContractsLoginResponse;
+use Illuminate\Support\Facades\Auth;
 
 class LoginResponse implements ContractsLoginResponse
 {
 	public function toResponse($request)
 	{
 		// here i am checking if the currently logout in users has a role_id of 2 which make him a regular user and then redirect to the users dashboard else the admin dashboard
-		if (auth()->user()->id == 1) {
-			return redirect()->route('dashboard');
-		} else {
-			return redirect()->route('home.index');
-		}
+		// if (auth()->user()->id == 1) {
+		// 	return redirect()->route('dashboard');
+		// } else {
+		// 	return redirect()->route('home.index');
+		// }
 		//return redirect()->intended(config('fortify.admin.home'));
+
+		/* by tonn */
+		$role= Auth::user()->roles->pluck('name')->all();
+		switch ($role[0]) {
+			case 'root':
+			case 'admin':
+				return redirect()->route('admin.index');
+				break;
+			case 'customer':
+				return redirect()->route('customer.index');
+				break;
+			default:
+				return redirect()->route('logout');
+				break;
+		}
 	}
 }
