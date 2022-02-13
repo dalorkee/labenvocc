16e91b31ba4d6a12c867527c1b72287af5764ff2
dalorkee <dalorkee@gmail.com>
2022-02-13 13:59:04 +0700

    install maat excel

--- config/app.php
+++ config/app.php
@@ -169,6 +169,7 @@ return [
 		 * Package Service Providers...
 		 */
 		Spatie\Permission\PermissionServiceProvider::class,
+		Maatwebsite\Excel\ExcelServiceProvider::class,
 		/*
 		 * Application Service Providers...
 		 */
@@ -233,6 +234,7 @@ return [
 		'Validator' => Illuminate\Support\Facades\Validator::class,
 		'View' => Illuminate\Support\Facades\View::class,
 		'DataTables' => Yajra\DataTables\Facades\DataTables::class,
+		'Excel' => Maatwebsite\Excel\Facades\Excel::class,
 	],
 
 ];
