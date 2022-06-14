8ecec78758f1c649947e24043996446ef3a10f2e
dalorkee <dalorkee@gmail.com>
2022-06-14 13:17:51 +0700

    resolve root home

--- resources/views/layouts/style.blade.php
+++ resources/views/layouts/style.blade.php
@@ -15,5 +15,9 @@
 	<link rel="stylesheet" type="text/css" href="{{ URL::asset('assets/css/vendors.bundle.for.staff.css') }}" media="screen, print">
 	<link rel="stylesheet" type="text/css" href="{{ URL::asset('assets/css/app.bundle.for.staff.css') }}" media="screen, print">
 @endrole
+@role('root')
+	<link rel="stylesheet" type="text/css" href="{{ URL::asset('assets/css/vendors.bundle.for.staff.css') }}" media="screen, print">
+	<link rel="stylesheet" type="text/css" href="{{ URL::asset('assets/css/app.bundle.for.staff.css') }}" media="screen, print">
+@endrole
 <link rel="stylesheet" type="text/css" href="{{ URL::asset('assets/css/notifications/toastr/toastr.css') }}" media="screen, print">
 <link rel="stylesheet" type="text/css" href="{{ URL::asset('assets/css/notifications/sweetalert2/sweetalert2.bundle.css') }}" media="screen, print">
