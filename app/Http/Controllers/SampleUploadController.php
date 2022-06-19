e173e304d4085fa774717927db0036bb2cd8da6d
gatjung <gatjunk@hotmail.com>
2022-06-19 17:05:39 +0700

    edit upload Excel control

--- app/Http/Controllers/SampleUploadController.php
+++ app/Http/Controllers/SampleUploadController.php
@@ -5,7 +5,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Maatwebsite\Excel\Facades\Excel;
 use App\Imports\SampleUploadImport;
-use App\Models\SampleUpload;
+use App\Models\{SampleUpload, Order};
 use App\Http\Requests\biouploadRequest;
 // use App\DataTables\UploadBioDataTable;
 
@@ -36,7 +36,37 @@ class SampleUploadController extends Controller
     * @return \Illuminate\Support\Collection
     */
     public function bioimport(biouploadRequest $request){
-        dd($request);
+        $auth = $this->user->load(['userCustomer']);
+        switch($request->type_of_work){
+            case('1');
+                $type_of_work_name = 'บริการ';
+            break;
+            case('2');
+                $type_of_work_name = 'วิจัย';
+            break;
+            case('3');
+                $type_of_work_name = 'เฝ้าระวัง';
+            break;
+            case('4');
+                $type_of_work_name = 'SRRT/สอบสวนโรค';
+            break;
+            case('5');
+                $type_of_work_name = 'อื่นๆ';
+            break;
+        }
+        $requestForm = new Order;
+        $requestForm->order_type = $request->order_type;
+        $requestForm->order_type_name = $request->order_type_name;
+        $requestForm->user_id = $auth->userCustomer->user_id;
+        $requestForm->customer_type = $auth->userCustomer->customer_type;
+        $requestForm->customer_agency_code = $auth->userCustomer->agency_code;
+        $requestForm->customer_agency_name = $request->customer_name;
+        $requestForm->type_of_work = $request->type_of_work;
+        $requestForm->type_of_work_name = $type_of_work_name;
+        $requestForm->type_of_work_other = $request->type_of_work_other;
+
+        dd($requestForm);
+
         Excel::import(new SampleUploadImport,$request->file('uploadbio'));
         // return redirect('import-excel-csv')->with('status', 'The file has been imported in laravel 8');
         return redirect()->back()->with('success','upload OK');
